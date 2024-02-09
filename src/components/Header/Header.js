2ea01162234966bb9b78eefff4dde7f867822a92
Ameur mohamed ayoub <2703320@collegelacite.ca>
2024-02-09 04:34:52 -0500

    update Navbar.js
    update Navbar.js and Header.js to be responsive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,23 +5,21 @@ import { TbGridDots } from "react-icons/tb";
 import Navbar from './Navbar';
 
 function Header() {
-    const [isNavbarVisible, setIsNavbarVisible] = useState(true);
-
-    const toggleNavbar = () => {
-        console.log(isNavbarVisible);
-        setIsNavbarVisible(!isNavbarVisible);
-    };
+    const [showMenu, setShowMenu] = useState(false);
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+      };
     return (
-
+       
         <div className="header flex" >
             <div className='logoDiv'>
                 <img src={logo} alt='' className='logo' />
             </div>
-            {isNavbarVisible && <Navbar />}
+           <Navbar menuStats={showMenu} />
 
             <div className="signUp flex ">
                 <div className="text btn primaryBtn">Sign Up</div>
-                <TbGridDots className="icon gridDotsIcon" onClick={toggleNavbar}  />
+                <TbGridDots className="icon gridDotsIcon" onClick={toggleMenu}   />
 
             </div>
         </div>
--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -1,24 +1,41 @@
+import React, { useEffect,useState } from 'react';
+
 import { IoIosCloseCircle } from "react-icons/io";
 
 
-function Navbar() {
-    return (<div className='navbar'>
+function Navbar(props) {
+    const [showMenu, setShowMenu] = useState(props.menuStats);
+    useEffect(() => {
+        setShowMenu(props.menuStats);
+    }, [props.menuStats]);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+        console.log(showMenu);
+    };
+
+    const closeMenuOnMobile = () => {
+        if (window.innerWidth <= 767) {
+          setShowMenu(false);
+        }
+      };
+    const navbar = <div className={`navbar ${showMenu ? "show" : "hidden"}`}>
         <ul className="menu">
             <li className="listItem">
-                <a href="/" className='link'>Used Cars</a>
+                <a href="/" className='link' onClick={closeMenuOnMobile}>Used Cars</a>
             </li>
             <li className="listItem">
-                <a href="/" className='link'>New Cars</a>
+                <a href="/" className='link' onClick={closeMenuOnMobile}>New Cars</a>
             </li>
             <li className="listItem">
-                <a href="/" className='link'>Aucation</a>
+                <a href="/" className='link' onClick={closeMenuOnMobile}>Aucation</a>
             </li>
             <li className="listItem">
-                <a href="/" className='link'>Sellers</a>
+                <a href="/" className='link' onClick={closeMenuOnMobile}>Sellers</a>
             </li>
         </ul>
-        <IoIosCloseCircle className="icon closeIcon" />
-
-    </div>)
+        <IoIosCloseCircle className="icon closeIcon" onClick={toggleMenu} />
+    </div>
+    return navbar;
 }
 export default Navbar
